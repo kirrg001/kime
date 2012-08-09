d23f894b0c406290abc0a86631f5363f3cf2a563
Katharina Irrgang <katharinairrgang@Katharina-Irrgangs-MacBook-Pro.local>
2012-08-09 17:17:08 +0200

    add layout.html

--- kime/settings.py
+++ kime/settings.py
@@ -106,9 +106,7 @@ ROOT_URLCONF = 'kime.urls'
 WSGI_APPLICATION = 'kime.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -118,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kime'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- kime/urls.py
+++ kime/urls.py
@@ -1,17 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'kime.views.home', name='home'),
-    # url(r'^kime/', include('kime.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'kime.views.home', name='home'),
 )
